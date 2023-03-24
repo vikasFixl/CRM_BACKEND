992542361f23f20a8de3e27653c74585ccd8ce73
fixlTech <tech@fixlsolutions.com>
2023-03-24 16:07:06 +0530

    R2

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -31,8 +31,7 @@ exports.signin = async (req, res) => {
     });
     await User.findByIdAndUpdate(user._id, { accessToken });
     if (user.role === "Admin") {
-      const orgDetails = await Org.findOne({ orgEmail: email });
-      //console.log(orgDetails);
+      const orgDetails = await Org.find({ orgEmail: email });
       res.status(200).json({
         data: {
           orgID: orgDetails._id,
