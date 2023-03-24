cf30ef1fd0102470ce06784175f805660dd1959c
fixlTech <tech@fixlsolutions.com>
2023-03-24 18:17:50 +0530

    user module orgId added

--- src/models/employeeModel.js
+++ src/models/employeeModel.js
@@ -1,78 +1,86 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema({
-    name:{
-        type : String
+  name: {
+    type: String,
+  },
+  orgId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ORG",
+  },
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  eid: {
+    type: String,
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  dob: {
+    type: String,
+  },
+  doj: {
+    type: String,
+  },
+  dol: {
+    type: Number,
+  },
+  designation: {
+    type: String,
+  },
+  panno: {
+    type: String,
+  },
+  bankDetails: {
+    accno: {
+      type: String,
     },
-    userid:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+    ifsc: {
+      type: String,
     },
-    eid :{
-        type: String,
-        unique : true
+    bname: {
+      type: String,
     },
-    gender :{
-        type: String
+  },
+  totalWorkingDays: {
+    type: Number,
+  },
+  month: [
+    {
+      jan: { type: Number },
+      feb: { type: Number },
+      mar: { type: Number },
+      apr: { type: Number },
+      may: { type: Number },
+      jun: { type: Number },
+      jul: { type: Number },
+      aug: { type: Number },
+      sep: { type: Number },
+      oct: { type: Number },
+      nov: { type: Number },
+      dec: { type: Number },
     },
-    dob :{
-        type:String
+  ],
+  leaves: [
+    {
+      jan: { type: Number },
+      feb: { type: Number },
+      mar: { type: Number },
+      apr: { type: Number },
+      may: { type: Number },
+      jun: { type: Number },
+      jul: { type: Number },
+      aug: { type: Number },
+      sep: { type: Number },
+      oct: { type: Number },
+      nov: { type: Number },
+      dec: { type: Number },
     },
-    doj :{
-        type:String
-    },
-    dol :{
-        type: Number
-    },
-    designation :{
-        type : String
-    },
-    panno:{
-        type:String
-    },
-    bankDetails:{
-        accno:{
-            type:String
-        },
-        ifsc:{
-            type:String
-        },
-        bname:{
-            type:String
-        },
-    },
-    totalWorkingDays:{
-        type:Number
-    },
-    month:[{
-        "jan":{type:Number},
-        "feb":{type:Number},
-        "mar":{type:Number},
-        "apr":{type:Number},
-        "may":{type:Number},
-        "jun":{type:Number},
-        "jul":{type:Number},
-        "aug":{type:Number},
-        "sep":{type:Number},
-        "oct":{type:Number},
-        "nov":{type:Number},
-        "dec":{type:Number}
-    }],
-    leaves:[{
-        "jan":{type:Number},
-        "feb":{type:Number},
-        "mar":{type:Number},
-        "apr":{type:Number},
-        "may":{type:Number},
-        "jun":{type:Number},
-        "jul":{type:Number},
-        "aug":{type:Number},
-        "sep":{type:Number},
-        "oct":{type:Number},
-        "nov":{type:Number},
-        "dec":{type:Number}
-    }]
+  ],
 });
 
-const employee = mongoose.model('employee',employeeSchema);
+const employee = mongoose.model("employee", employeeSchema);
 module.exports = employee;
