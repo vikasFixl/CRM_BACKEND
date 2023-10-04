c244d6886239c554e1c018d2f0cfa5b9e640066f
Niteshgairola01 <nitesh@fixlsolutions.com>
2023-10-04 15:39:16 +0530

    details added in reminder model

--- src/models/ReminderModel.js
+++ src/models/ReminderModel.js
@@ -3,7 +3,8 @@ const ReminderSchema = new mongoose.Schema(
   {
     date: { type: Date },
     time: { type: String },
-    text: { type: String }
+    text: { type: String },
+    details: { type: String }
   },
   {
     timestamps: true,
