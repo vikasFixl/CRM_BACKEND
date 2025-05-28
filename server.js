53f176b4e3f6ef3edff853b842f6b24801eefe58
vikasFixl <vikasfixl@gmail.com>
2025-05-28 13:21:26 +0530

    fixed the cors error

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(
   {
-    origin: "http://localhost:3000",
+    origin:  "http://localhost:5173",
     credentials: true
   }
 ));
