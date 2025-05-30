72e1618f8fd286104a05a9baa3c467346ed5e940
vikasFixl <vikasfixl@gmail.com>
2025-05-30 16:21:03 +0530

    changed cors config

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // cors configuration
 app.use(cors(
   {
-    origin:  "http://localhost:5173",
+    origin:  "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }
