34391e33b2f6c3c0245529547865eea4bb5e7db6
vikasFixl <vikasfixl@gmail.com>
2025-06-16 18:28:20 +0530

    updated client controller

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -25,7 +25,7 @@ export const createClient = async (req, res) => {
       taxId,
       tinNo,
       cinNo,
-      
+
       firmId,
     } = client.data;
 
@@ -69,41 +69,47 @@ export const createClient = async (req, res) => {
   }
 };
 
-// exports.getClientById = async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const client = await ClientModel.findById(id);
-//     res.status(200).json({
-//       data: client,
-//       success: true,
-//       code: 200,
-//       message: "fetch client by id successfully!!",
-//     });
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// };
+export const getClientById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ message: "Invalid id" });
+    }
+    const client = await ClientModel.findById(id);
+    if (!client) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+    res.status(200).json({
+      data: client,
+      success: true,
+      code: 200,
+      message: "fetch client by id successfully!!",
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
-// exports.getClients = async (req, res) => {
-//   const { page } = req.query;
-//   const { orgId } = req.params;
-//   try {
-//     const LIMIT = 10;
-//     const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
-//     const total = await ClientModel.countDocuments({ orgId: orgId });
-//     const clients = await ClientModel.find({ orgId: orgId })
-//       .sort({ _id: -1 })
-//       .limit(LIMIT)
-//       .skip(startIndex);
-//     res.json({
-//       data: clients,
-//       currentPage: Number(page),
-//       numberOfPages: Math.ceil(total / LIMIT),
-//     });
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// };
+export const getClients = async (req, res) => {
+  const { page } = req.query;
+  const orgId = req.orgUser.orgId;
+  try {
+    const LIMIT = 10;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    const total = await ClientModel.countDocuments({ orgId: orgId });
+    const clients = await ClientModel.find({ orgId: orgId })
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+    res.json({
+      data: clients,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 // exports.updateClient = async (req, res) => {
 //   const { id: _id } = req.params;
@@ -124,24 +130,24 @@ export const createClient = async (req, res) => {
 //   });
 // };
 
-// exports.deleteClient = async (req, res) => {
-//   const { id } = req.params;
-//   if (!mongoose.Types.ObjectId.isValid(id))
-//     return res.status(404).send("No Client with that id");
-//   await ClientModel.findByIdAndRemove(id);
-//   res.json({
-//     code: 201,
-//     success: true,
-//     message: "Client deleted successfully",
-//   });
-// };
+export const deleteClient = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("invalid id");
+  await ClientModel.findByIdAndRemove(id);
+  res.json({
+    code: 201,
+    success: true,
+    message: "Client deleted successfully",
+  });
+};
 
-// exports.getClientsByUser = async (req, res) => {
-//   const { searchQuery } = req.query;
-//   try {
-//     const clients = await ClientModel.find({ userId: searchQuery });
-//     res.json({ data: clients });
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// };
+export const getClientsByUser = async (req, res) => {
+  const { searchQuery } = req.query;
+  try {
+    const clients = await ClientModel.find({ userId: searchQuery });
+    res.json({ data: clients });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
