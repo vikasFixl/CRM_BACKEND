7f6f35a0497d9a238302aa955141c9044c3b0011
vikasFixl <vikasfixl@gmail.com>
2025-07-11 16:10:37 +0530

    updated role adn permission

--- src/models/RolePermission.js
+++ src/models/RolePermission.js
@@ -1,43 +1,35 @@
 import mongoose from "mongoose";
-// import { ROLE_SCOPE } from "../../enums/roleScope.enum.js";
-export const ROLE_SCOPE = {
-  ORGANIZATION: "x1o",  // org-level
-  WORKSPACE: "x2w",     // workspace-level
-  PROJECT: "x3p",       // project-level
-  TEAM: "x4t",          // team-level
-};
-export const SCOPE_LABELS = {
-  x1o: "organization",
-  x2w: "workspace",
-  x3p: "project",
-  x4t: "team",
-};
+import { ROLES, MODULES, PERMISSIONS, ROLE_SCOPE } from "../enums/role.enums.js";
+
 const rolePermissionSchema = new mongoose.Schema(
   {
     orgId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Organization",
-      required: true,
       index: true,
     },
+
     workspaceId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Workspace",
       default: null,
     },
-    projectId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Project",
-      default: null,
-    },
-    teamId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Team",
-      default: null,
-    },
+
+    // projectId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Project",
+    //   default: null,
+    // },
+
+    // teamId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Team",
+    //   default: null,
+    // },
 
     role: {
       type: String,
+      enum: Object.values(ROLES),
       required: true,
     },
 
@@ -59,8 +51,18 @@ const rolePermissionSchema = new mongoose.Schema(
 
     permissions: [
       {
-        module: String, // or use ENUM if you have one
-        actions: [String],
+        module: {
+          type: String,
+          enum: Object.values(MODULES),
+          required: true,
+        },
+        actions: [
+          {
+            type: String,
+            enum: Object.values(PERMISSIONS),
+            required: true,
+          },
+        ],
       },
     ],
   },
