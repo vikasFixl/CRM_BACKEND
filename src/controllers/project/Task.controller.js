714bd94d52d01c9fff63393b36a755fcab17caeb
vikasFixl <vikasfixl@gmail.com>
2025-07-15 12:47:22 +0530

    lead validation

--- src/controllers/project/Task.controller.js
+++ src/controllers/project/Task.controller.js
@@ -291,7 +291,7 @@ export const updateTask = async (req, res) => {
     if (!task) {
       await session.abortTransaction();
       session.endSession();
-      return res.status(404).json({ message: "Task not found" });
+      return res.status(404).json({ message: "Task not found or task not part of project" });
     }
 
     // Handle status update if present in request
